fc0f61fe3baba70fc11d37548f968c184efb83b3
aris <makehead15@gmail.com>
2026-07-26 10:07:40 +0900

    [level 1] Title: 기사단원의 무기, Time: 7799.76 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        for (int i = 1; i<= number; i++){
+            int count = 0;
+            for (int j = 1; j <= i; j++){
+                if (i % j == 0){
+                    count+=1;
+                }
+            }
+            if (count > limit){
+                count = power;
+            }
+            answer += count;
+        }
+        return answer;
+    }
+}
