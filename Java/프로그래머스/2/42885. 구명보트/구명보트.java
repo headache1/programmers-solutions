7882924dc36d6cb145f4043be220eab517cb063b
aris <makehead15@gmail.com>
2026-08-12 21:02:29 +0900

    [level 2] Title: 구명보트, Time: 42.23 ms, Memory: 61.7 MB -BaekjoonHub

--- Java/프로그래머스/2/42885. 구명보트/구명보트.java
+++ Java/프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,21 +2,37 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        Arrays.sort(people);
-
-        int left = 0;
-        int right = people.length - 1;
         int answer = 0;
-
-        while (left <= right) {
-            if (people[left] + people[right] <= limit) {
-                left++;
-            }
-
-            right--;
-            answer++;
+        ArrayList <Integer> list = new ArrayList<>();
+        for (int x : people){
+            list.add(x);
         }
-
-        return answer;
+        Collections.sort(list);
+        
+        int left = 0;
+        int right = list.size()-1;
+        
+        while (list.size() >=2){
+            if (list.get(left) + list.get(right) <= limit){
+                list.remove(right);
+                list.remove(left);
+                answer += 1;
+                right = list.size()-1;
+            }
+            else if (list.get(left) + list.get(right) > limit){
+                list.remove(right);
+                answer+=1;
+                right = list.size()-1;
+            }
+        }
+        return answer + list.size();
     }
 }
+
+/*
+구하고자 하는 것 : 구명보트 최소 개수
+
+의문점 : 30 30 50 70 으로 하면 순서대로 하면 3번이지만, 탐욕법으로는 2번
+
+해결 자료구조 : sort하고 왼쪽부터랑 오른쪽부터랑 따로 세서 
+*/
