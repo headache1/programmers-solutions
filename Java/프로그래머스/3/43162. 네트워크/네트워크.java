770726f7c7b07fab1fdc5b22cab870e0f979ee80
aris <makehead15@gmail.com>
2026-08-18 22:38:46 +0900

    [level 3] Title: 네트워크, Time: 0.13 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, computers, visited);
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public void dfs(int now, int[][] computers, boolean[] visited) {
+        visited[now] = true;
+
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[now][i] == 1 && !visited[i]) {
+                dfs(i, computers, visited);
+            }
+        }
+    }
+}
